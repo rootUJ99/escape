2f746a34aa8f007fab7778f3bbe61f9fceafad73
Ujwal <ujwal@symboinsurance.com>
2018-09-24 01:55:48 +0530

    playing with ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,59 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
-
+Interface Props = {
+  item: number
+}
+const SomeComponent:React.SFC<Props> = (props: any) => (
+<h1 className="Hello" key={props.item}>{props.item}</h1>
+);
 class App extends React.Component {
   public state ={
     activateShit: false,
-    someShittyArray: [1]
+
   }
-  public someHandler = () =>{
-    for (let i= 0; i<=10; i++){
-      console.log(i);
-      this.setState({someShittyArray: this.state.someShittyArray.push(this.state.someShittyArray[i])})
-      this.someOtherHandler();
-    }
-  }
+  // public someHandler = () =>{
+  //   for (let i= 0; i<=10; i++){
+  //     console.log(i);
+  //     this.setState({
+  //       someShittyArray: [...this.state.someShittyArray]
+  //     })
+  //     this.someOtherHandler();
+  //   }
+  // }
   public someOtherHandler = () =>{
-        setTimeout(()=>{
-          this.setState({activateShit:!this.state.activateShit})},
-           1000);
+        // setTimeout(()=>{
+          this.setState({activateShit:true})
+        // },
+        //    1000);
   }
   public render() {
+    const someShittyArray: any = [];
+    const handler =()=>{
+      for (let i= 0; i<=10; i++){
+        someShittyArray.push(
+        <SomeComponent
+        item={i}
+        />)
+
+        this.someOtherHandler();
+      }
+      console.log(someShittyArray);
+    }
+    
     // tslint:disable-next-line
     debugger;
-    console.log(typeof this.state.someShittyArray);
-    console.log(this.state.someShittyArray);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <button name="someButton" onClick={this.someHandler}>
+          <button name="someButton" onClick={handler}>
           clickHere
           </button>
         </header>
-          {this.state.activateShit && this.state.someShittyArray.map((a, index )=>  <h1 className="Hello" key={index}>{a}</h1>) }
+        {console.log(someShittyArray)}
+          {this.state.activateShit && someShittyArray.map((a:number)=>a) }
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -42,6 +62,5 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
